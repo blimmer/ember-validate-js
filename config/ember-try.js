cad08a107f1c565d3ec8c8c0a009d3841250e52e
Ben Limmer <ben@benlimmer.com>
2015-09-06 16:58:31 -0600

    Test with 1.12.x and 1.13.x. I know not everyone is onto Ember 2.0 yet, so show that we're working!

--- config/ember-try.js
+++ config/ember-try.js
@@ -5,6 +5,20 @@ module.exports = {
       dependencies: { }
     },
     {
+      name: 'ember-1.12.x',
+      dependencies: {
+        'ember': '~1.12.1',
+        'ember-data': '~1.13.11'
+      }
+    },
+    {
+      name: 'ember-1.13.x',
+      dependencies: {
+        'ember': '~1.13.10',
+        'ember-data': '~1.13.11'
+      }
+    },
+    {
       name: 'ember-release',
       dependencies: {
         'ember': 'components/ember#release'
